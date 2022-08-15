9c7822cb275a2795f41c4f7b1614f099bb72bd34
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-08-15 18:07:13 -0300

    add resolution desafio1

--- src/desafio1.ts
+++ src/desafio1.ts
@@ -1,5 +1,9 @@
 // Como podemos rodar isso em um arquivo .ts sem causar erros?
+interface EmployeeProps {
+  code: number;
+  name: string;
+}
 
-let employee = {};
+let employee = {} as EmployeeProps;
 employee.code = 10;
 employee.name = "John";
