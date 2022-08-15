7f69f61c728447eca4b63d878df57be3c0c17d19
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-08-15 18:11:10 -0300

    add Interface and prepare variables

--- src/desafio2.ts
+++ src/desafio2.ts
@@ -1,23 +1,29 @@
 // Como podemos melhorar o esse código usando TS?
 
-let pessoa1 = {};
+interface PersonalProps {
+  nome: string;
+  idade: number;
+  profissao: string;
+}
+
+let pessoa1 = {} as PersonalProps;
 pessoa1.nome = "maria";
 pessoa1.idade = 29;
 pessoa1.profissao = "atriz";
 
-let pessoa2 = {};
+let pessoa2 = {} as PersonalProps;
 pessoa2.nome = "roberto";
 pessoa2.idade = 19;
 pessoa2.profissao = "Padeiro";
 
-let pessoa3 = {
+let pessoa3: PersonalProps = {
   nome: "laura",
-  idade: "32",
+  idade: 32,
   profissao: "Atriz",
 };
 
-let pessoa4 = {
-  nome = "carlos",
-  idade = 19,
-  profissao = "padeiro",
+let pessoa4: PersonalProps = {
+  nome: "carlos",
+  idade: 19,
+  profissao: "padeiro",
 };
